5976f99536edb520bc734ca6532d6de1990735a6
kobayang <naokiod@gmail.com>
2017-03-05 13:09:17 +0900

    navをapplication共通のものに変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def show
+    @user = User.find(params[:user_id])
   end
 
   def edit
